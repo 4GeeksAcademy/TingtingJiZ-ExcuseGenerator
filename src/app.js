cfa2658520aa0faeed9f6fab5d3741bf11427f89
TingtingJiZ <168580008+TingtingJiZ@users.noreply.github.com>
2024-05-09 23:22:38 +0000

    excuses generator terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function randomIndex(items) {
+    return Math.floor(Math.random() * items.length);
+  }
+
+  let indexwho = randomIndex(who);
+  let indexaction = randomIndex(action);
+  let indexwhat = randomIndex(what);
+  let indexwhen = randomIndex(when);
+
+  let excuse =
+    who[indexwho] +
+    " " +
+    action[indexaction] +
+    " " +
+    what[indexwhat] +
+    " " +
+    when[indexwhen];
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
